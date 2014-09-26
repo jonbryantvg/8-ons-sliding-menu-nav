99dbd65ced8f43f1d088c85a80c37450bb3d7669
jon <jon.bryant@visioglobe.com>
2014-09-26 17:11:01 +0200

    modified the loader to detect windows phone devices

--- 8-ons-sliding-menu-nav/www/components/loader.js
+++ 8-ons-sliding-menu-nav/www/components/loader.js
@@ -9,21 +9,37 @@
 /*** <Start:monaca-cordova-loader> ***/
 /*** <Start:monaca-cordova-loader LoadJs:"components/monaca-cordova-loader/cordova-loader.js"> ***/
 (function(){
-  if ((navigator.userAgent.match(/Android/i)) || (navigator.userAgent.match(/iPhone|iPad|iPod/i))) {
+
+  if ((navigator.userAgent.match(/Android/i)) || (navigator.userAgent.match(/iPhone|iPad|iPod/i)) || (navigator.userAgent.match(/Windows Phone/i))) {
+
     if (typeof location.href === "string") {
+
       var relativePath = location.href.split("/www")[1];
+
       var paths = relativePath.split("/");
+
       var cordovaJsUrl = ""; 
+
       for (var i = 0; i < paths.length - 2; i++) {
+
         cordovaJsUrl += "../";
+
       }
+
       document.write("<script src=\"" + cordovaJsUrl+ "cordova.js" + "\"></script>");
+
     }
+
   } else if ((navigator.userAgent.match(/MSIE\s10.0/)) && (navigator.userAgent.match(/Windows\sNT\s6.2/))) {
+
     var elm = document.createElement('script');
+
     elm.setAttribute("src", "cordova.js");
+
     document.getElementsByTagName("head")[0].appendChild(elm);
+
   };
+
 })();
 /*** <End:monaca-cordova-loader LoadJs:"components/monaca-cordova-loader/cordova-loader.js"> ***/
 /*** <End:monaca-cordova-loader> ***/
